f7426acb9f751eb0a724139945b511149b3cc20c
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-06-16 19:50:57 -0400

    creacion microservicio reabastecimiento

---
+++ src/test/java/ecomarket/reabastecimiento_ms/ReabastecimientoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomarket.reabastecimiento_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReabastecimientoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
